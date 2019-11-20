480e7cd2a3ab60fa7e9dccd7ac48a19cc8ec2b44
Tracy M <45837651+TracyMDev@users.noreply.github.com>
2019-11-19 23:30:36 -0500

    Initial Commit

---
+++ KafkaKittenStore/KafkaKittenCatcher/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace KafkaKittenCatcher
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ConsumerConfig config = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "[CLOUDKARAFKA_BROKERS]",
+                SecurityProtocol = SecurityProtocol.SaslSsl,
+                SaslMechanism = SaslMechanism.ScramSha256,
+                SaslUsername = "[CLOUDKARAFKA_USERNAME]",
+                SaslPassword = "[CLOUDKARAFKA_PASSWORD]"
+            };
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe("[PREFIX]-kitten-topic");
+
+                CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) =>
+                  {
+                    e.Cancel = true; //prevent the process from terminating.
+                    cancellationTokenSource.Cancel();
+
+                  };
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = consumer.Consume(cancellationTokenSource.Token);
+                            Console.WriteLine($"Consumed message '{cr.Value}' at '{cr.TopicPartitionOffset}'.");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    //Ensure the consumer leaves the group cleanly and final offsets are committed.
+                    consumer.Close();
+                }
+
+            
+                
+            }
+        }
+    }
+}
---
+++ KafkaKittenStore/KafkaKittenStore/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace KafkaKittenStore
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+            ProducerConfig config = new ProducerConfig
+            {
+                BootstrapServers = "[CLOUDKARAFKA_BROKERS]",
+                SecurityProtocol = SecurityProtocol.SaslSsl,
+                SaslMechanism = SaslMechanism.ScramSha256,
+                SaslUsername = "[CLOUDKARAFKA_USERNAME]",
+                SaslPassword = "[CLOUDKARAFKA_PASSWORD]"
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    string stringValue = "";
+                    for (int i = 0; i < 5; ++i)
+                    {
+
+                        stringValue += "ฅ^•ﻌ•^ฅ";
+                        var deliveryReport = await producer.ProduceAsync(topic: "[PREFIX]-kitten-topic", message: new Message<Null, string> { Value = stringValue });
+                        Console.WriteLine($"Delivered {deliveryReport.Message.Value} to partition {deliveryReport.TopicPartitionOffset}");
+                    }
+                }
+                catch (ProduceException<Null, string> exception)
+                {
+
+                    Console.WriteLine($"Delivery failed: {exception.Error.Reason}");
+                }
+
+            }
+
+
+        }
+    }
+}
